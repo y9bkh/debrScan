85822a222c6e26c26985369eba5493325ae597fc
Younes <yboukhanamhani@gmail.com>
2026-01-07 00:33:04 +0100

    Añadir comentariso y mejora de sintaxis

--- debr.py
+++ debr.py
@@ -1,16 +1,20 @@
 import socket
 
+# Función que recibe el host y el puerto TCP y devolverá un boolean
 def debrScan(host, port):
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creamos un socket TCP IPv4 
     cliente.settimeout(10)
+
+    # Determina si el puerto esta abierto o cerrado mediante la conexión
     try:
         cliente.connect((host,port))
-        cliente.close
         return True
     except socket.error:
-        socket.close
         return False
-    
+    finally:
+        cliente.close()
+
+# Solicita al usuario el host y puerto a analizar
 host = input("Introduce el host a escanear: ")
 port = int(input("Introduce el puerto a escanear: "))
 
